9ca7632be286bfd55879ade89b1ba9e319d71d37
aishwarya <ashp17star@gmail.com>
2026-06-30 12:56:40 +0530

    Recycler View with ViewType

--- app/src/main/java/com/example/a25_05_2026_recyclerview_viewtype/MainActivity.java
+++ app/src/main/java/com/example/a25_05_2026_recyclerview_viewtype/MainActivity.java
@@ -7,18 +7,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    LinearLayoutManager linearLayoutManager;
+    ArrayList<User> users;
+    ArrayList<Product> products;
+    ProductAndUserAdapter productAndUserAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        initArrays();
+        initViews();
+    }
+
+    public void initViews() {
+        recyclerView = findViewById(R.id.recyclerViewForProductsUsers);
+        productAndUserAdapter = new ProductAndUserAdapter(users, products);
+        recyclerView.setAdapter(productAndUserAdapter);
+        linearLayoutManager = new LinearLayoutManager(
+                this,
+                LinearLayoutManager.VERTICAL,
+                false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    public void initArrays() {
+        users = new ArrayList<User>();
+        products = new ArrayList<Product>();
+
+        for (int i = 0; i < 10; i++) {
+            products.add(
+                    new Product(
+                            i + 10,
+                            "Product " + i,
+                            i * 1000 + 500)
+            );
+        }
+
+        for (int i = 0; i < 5; i++) {
+            users.add(new User(
+                    i + 5,
+                    "User " + i,
+                    "Pune")
+            );
+        }
     }
 }
